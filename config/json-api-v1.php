bd0eddd820d4adfaeab501482683da4eb7f95f5a
Eliezer Orellana <eli007s@gmail.com>
2021-03-26 18:56:22 -0700

    lesson 13

---
+++ config/json-api-v1.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'resolver' => \CloudCreativity\LaravelJsonApi\Resolver\ResolverFactory::class,
+    'namespace' => null,
+    'by-resource' => true,
+    'model-namespace' => 'App\Models',
+    'resources' => [
+        'article' => \App\Models\Article::class,
+    ],
+    'use-eloquent' => true,
+    'url' => [
+        'host' => null,
+        'namespace' => '/api/v1',
+        'name' => 'api.v1.',
+    ],
+    'controllers' => [
+        'transactions' => true,
+        'connection' => null,
+    ],
+    'jobs' => [
+        'resource' => 'queue-jobs',
+        'model' => \CloudCreativity\LaravelJsonApi\Queue\ClientJob::class,
+    ],
+    'encoding' => [
+        'application/vnd.api+json',
+    ],
+    'decoding' => [
+        'application/vnd.api+json',
+    ],
+    'providers' => [],
+];
