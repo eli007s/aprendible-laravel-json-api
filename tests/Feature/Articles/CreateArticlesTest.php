8d2433a4ecedc6bd7afd7b9b224c9406225e7f3c
Eliezer Orellana <eli007s@gmail.com>
2021-03-27 14:59:40 -0700

    lesson 15

--- tests/Feature/Articles/CreateArticlesTest.php
+++ tests/Feature/Articles/CreateArticlesTest.php
@@ -22,7 +22,109 @@ class CreateArticlesTest extends TestCase
         $this->assertDatabaseMissing('articles', $article);
 
         $this->jsonApi()
-            ->content()
-            ->post($url);
+            ->content([
+                'data' => [
+                    'type' => 'articles',
+                    'attributes' => $article
+                ]
+            ])
+            ->post($url)
+            ->assertCreated();
+
+        $this->assertDatabaseHas('articles', $article);
+    }
+
+    /**
+     * @test
+     */
+    public function it_has_required_title(): void
+    {
+        $article = Article::factory()->raw(['title' => '']);
+
+        $url = route('api.v1.articles.create');
+
+        $this->jsonApi()
+            ->content([
+                'data' => [
+                    'type' => 'articles',
+                    'attributes' => $article
+                ]
+            ])
+            ->post($url)
+            ->assertStatus(422)
+            ->assertSee('data\/attributes\/title');
+
+        $this->assertDatabaseMissing('articles', $article);
+    }
+
+    /**
+     * @test
+     */
+    public function it_has_required_slug(): void
+    {
+        $article = Article::factory()->raw(['slug' => '']);
+
+        $url = route('api.v1.articles.create');
+
+        $this->jsonApi()
+            ->content([
+                'data' => [
+                    'type' => 'articles',
+                    'attributes' => $article
+                ]
+            ])
+            ->post($url)
+            ->assertStatus(422)
+            ->assertSee('data\/attributes\/slug');
+
+        $this->assertDatabaseMissing('articles', $article);
+    }
+
+    /**
+     * @test
+     */
+    public function it_has_unique_slug(): void
+    {
+        Article::factory()->create(['slug' => 'some-slug']);
+
+        $article = Article::factory()->raw(['slug' => 'some-slug']);
+
+        $url = route('api.v1.articles.create');
+
+        $this->jsonApi()
+            ->content([
+                'data' => [
+                    'type' => 'articles',
+                    'attributes' => $article
+                ]
+            ])
+            ->post($url)
+            ->assertStatus(422)
+            ->assertSee('data\/attributes\/slug');
+
+        $this->assertDatabaseMissing('articles', $article);
+    }
+
+    /**
+     * @test
+     */
+    public function it_has_required_content(): void
+    {
+        $article = Article::factory()->raw(['content' => '']);
+
+        $url = route('api.v1.articles.create');
+
+        $this->jsonApi()
+            ->content([
+                'data' => [
+                    'type' => 'articles',
+                    'attributes' => $article
+                ]
+            ])
+            ->post($url)
+            ->assertStatus(422)
+            ->assertSee('data\/attributes\/content');
+
+        $this->assertDatabaseMissing('articles', $article);
     }
 }
